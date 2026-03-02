a36505a34eb1741177210c9c618efcd1a8aadbe6
teslaafric <sobi@byupathway.edu>
2026-03-02 20:13:48 +0100

    Added a while loop and calling random object

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Globalization;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+       
+        // int randomNumber = 0;
+        string response = "yes";
+        Random  randomNumber = new Random();
+      
+
+        // Console.Write("Enter a random number: ");
+        // string number = Console.ReadLine();
+        // randomNumber = int.Parse(number);
+        
+        while (response == "yes")
+        {  
+            int guessNumber = -1;
+            int guessCount = 0;
+            int number = randomNumber.Next(1, 101);
+
+            while (guessNumber != number)
+            {
+                Console.Write("What is your guess number: ");
+                string guess = Console.ReadLine();
+                guessNumber = int.Parse(guess);
+                guessCount ++;
+
+                if (guessNumber > number)
+                {
+                    Console.WriteLine("The guess number is higher! Go lower.");
+                }
+                else if (guessNumber < number)
+                {
+                    Console.WriteLine("The guess number is lower! Go higher.");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed right! {guessCount} guesses"); 
+                }
+            }   
+            Console.Write("Would you like to play again? yes/no: ");
+            response = Console.ReadLine().ToLower();
+
+        }       
+        Console.WriteLine("Goodbye");
+            
     }
 }
