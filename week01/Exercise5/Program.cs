1341b3a1f1bf98799bffc5b9fce92e0a11ef0a16
teslaafric <sobi@byupathway.edu>
2026-03-03 21:08:07 +0100

    Created functions, calling each and passing parameter and argument

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string name =  PromptUserName();
+        int number = PromptUserNumber();
+        int squared = SquareNumber(number);
+        DisplayResult(name, squared);
+
     }
+    
+    static void DisplayWelcome ()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName ()
+    {
+        Console.Write("What is your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int PromptUserNumber ()
+    {
+        Console.Write("What is your favourite number: ");
+        int number =int.Parse(Console.ReadLine());
+        return number;
+    }
+    static int SquareNumber (int number)
+    {
+        return number * number;
+    }
+    static void DisplayResult (string userName, int squaredNumber)
+    {
+        Console.Write($"{userName}, the square of your favourite number is {squaredNumber}");
+    }
+
 }
