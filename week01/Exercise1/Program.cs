028482a6aa843f56814b490809492d0843a73462
teslaafric <sobi@byupathway.edu>
2026-03-08 23:16:07 +0100

    Corrected the variable names to standard convention

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -7,11 +7,11 @@ class Program
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
 
         Console.Write("What is your first name: ");
-        string firstname = Console.ReadLine();
+        string firstName = Console.ReadLine();
         Console.Write("What is your last name: ");
-        string lastname = Console.ReadLine();
+        string lastName = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
         
 
     }
