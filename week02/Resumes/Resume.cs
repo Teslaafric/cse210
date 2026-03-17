56392cd3f05de04f3131249cb543fe7fb9b17adb
teslaafric <sobi@byupathway.edu>
2026-03-17 17:00:12 +0100

    Apply abstraction using classes, loop, member variables and objects and method

---
+++ week02/Resumes/Resume.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+public class Resume
+{
+  public string _firstName;
+  public string _lastName;
+
+  public List<Job> _jobs = new List<Job>();
+
+  public void Display()
+  {
+     Console.WriteLine($"Name: {_firstName} {_lastName}");
+     Console.WriteLine("Jobs: ");
+    foreach (Job job in _jobs)
+    {
+      job.Display();
+    }
+     
+  }
+
+}
