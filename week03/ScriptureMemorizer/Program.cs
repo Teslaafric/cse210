6a32bf77b3859866f7f425dffd4933393d24a15b
teslaafric <sobi@byupathway.edu>
2026-03-28 23:49:47 +0100

    Encapsulation added in this program

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -5,5 +5,32 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+
+        Reference reference = new Reference("Alma", 5, 14);
+
+        Scripture scripture = new Scripture(reference,
+            "And now behold, I ask of you, my brethren of the church, have ye spiritually been born of God? Have ye received his image in your countenances? Have ye experienced this mighty change in your hearts?");
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+
+            if (scripture.CompletelyHidden())
+            {
+                break;
+            }
+
+            Console.WriteLine("Press Enter to continue or type 'quit' to exit:");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+            {
+                break;
+            }
+
+            scripture.HideRandomWords(4);
+        }
     }
+    
 }
